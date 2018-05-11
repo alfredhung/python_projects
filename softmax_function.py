94aa2a1d8b40c456a20d75b120fd9aa146870af1
Alfred K. Hung <akh022@gmail.com>
2018-05-11 17:20:28 -0400

    Fix: Change text lines

--- softmax_function.py
+++ softmax_function.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 Softmax function
+We implement the softmax function
 """
 import numpy as np
 
@@ -13,26 +13,27 @@ def softmax(L):
         p.append(en)
     return p
 
-# alternative SOLUTION
+# Alternative SOLUTION 1
 import numpy as np
 
-def softmax(L):
-    expL = np.exp(L)
-    sumExpL = sum(expL)
-    result = []
-    for i in expL:
-        result.append(i*1.0/sumExpL)
-    return result
-    
-    # Note: The function np.divide can also be used here, as follows:
-    # def softmax(L):
-    #     expL = np.exp(L)
-    #     return np.divide (expL, expL.sum())
+#def softmax(L):
+#    expL = np.exp(L)
+#    sumExpL = sum(expL)
+#    result = []
+#    for i in expL:
+#        result.append(i*1.0/sumExpL)
+#    return result
+ 
+# Alternative SOLUTION 1
 
-#Trying for L=[5,6,7]. 
+#def softmax(L):
+#    expL = np.exp(L)
+#    return np.divide (expL, expL.sum())
+
+L=[5,6,7]
 #The correct answer is [0.09003057317038046, 0.24472847105479764, 0.6652409557748219] 
-#And your code returned [0.09003057317038046, 0.24472847105479764, 0.6652409557748219] 
-#Correct!
+#My code returned [0.09003057317038046, 0.24472847105479764, 0.6652409557748219] 
+
 
 # 2. Calculate location of points in blue/red zone using Sigmoid:
 from math import exp
