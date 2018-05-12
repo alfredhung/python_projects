af86c609c7545ccedb8c4b38417425f362440a7f
Alfred K. Hung <akh022@gmail.com>
2018-05-12 15:44:19 -0400

    refactor: Change softmax function

--- softmax_function.py
+++ softmax_function.py
@@ -6,23 +6,23 @@ import numpy as np
 
 # 1. Function that takes as input a list of numbers, and returns
 # the list of values given by the softmax function.
-def softmax(L):
-    p = []
-    for i in xrange(len(L)):    
-        en = np.exp(L[i]) / np.sum(np.exp(L))
-        p.append(en)
-    return p
+#def softmax(L):
+#    p = []
+#    for i in xrange(len(L)):    
+#        en = np.exp(L[i]) / np.sum(np.exp(L))
+#        p.append(en)
+#    return p
 
 # Alternative SOLUTION 1
 import numpy as np
 
-#def softmax(L):
-#    expL = np.exp(L)
-#    sumExpL = sum(expL)
-#    result = []
-#    for i in expL:
-#        result.append(i*1.0/sumExpL)
-#    return result
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i*1.0/sumExpL)
+    return result
  
 # Alternative SOLUTION 1
 
